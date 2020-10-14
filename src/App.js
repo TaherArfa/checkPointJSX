d51cfb084ced1eb307bd57d3a6671f10cef934ef
TaherArfa <taherarfaoui@gmail.com>
2020-10-15 00:49:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import tunisia1 from "./img/tunisia1.jpg";
+// import tunisia2 from "./public/tunisia2.jpg";
+import "./App.css";
+import "./css/style.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +20,35 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
+          {/* style="border:solid 1px black;max-width:100vw"   */}
+          <div className="mainDiv">
+          <h1 className="title red">Visite Tunisia</h1>
+
+          {/* <br > */}
+          <div className="imgDiv">
+          <img src={tunisia1} className="firstImg" alt="firstImg" />
+
+          {/* </br > */}
+
+          <img src="/tunisia2.jpg" className="secondImg" alt="secondImg"/>
+        </div>
+        </div>
+
+        {/* width="320" height="240" controls */}
+        {/* <video  src={myVideo.mp4} type="video/mp4" /> */}
+
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/OtJVufo3IrA"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          title="video"
+          className="videoo"
+        ></iframe>
       </header>
     </div>
   );
